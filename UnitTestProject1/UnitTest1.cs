99aa90ceddf16e4eef7a715e68524b8fc7e3864c
lekip <kasp964n@edu.eal.dk>
2017-12-09 12:30:55 +0100

    Created unit test project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tidsregistrering;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calc c = new Calc();
+            int r = 2 + 2;
+            Assert.AreEqual(c.add(2, 2), r);
+            
+        }
+    }
+}
